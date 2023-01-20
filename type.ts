9ed7795b1530c736bc3fbc2058b68e456931be2b
srinivas5b <123141881+srinivas5b@users.noreply.github.com>
2023-01-20 10:27:45 +0530

    type.ts
    typescript

---
+++ type.ts
@@ -0,0 +1,14 @@
+for(let i = 2;i<=100;i++){
+    let flag = 0;
+    
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (i > 1 && flag == 0) {
+        console.log(i);
+    }
+}
